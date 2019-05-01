58e21c1ce41a09d4671a79083ac4ca0716171fcf
kristian Vincentzen <kvincentzen@gmail.com>
2019-05-01 13:02:40 +0200

    EDD

--- DateApp/Program.cs
+++ DateApp/Program.cs
@@ -10,6 +10,8 @@ namespace DateApp
     {
         static void Main(string[] args)
         {
+            SqlCommands.addPerson("Kristian");
+            Console.ReadKey();
         }
     }
 }
--- DateApp/SqlCommands.cs
+++ DateApp/SqlCommands.cs
@@ -3,17 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data;
-using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
 
 namespace DateApp
 {
     class SqlCommands
     {
-        static void addPerson(string Name)
+        public static void addPerson(string Name)
         {
-            var connection = new SqlConnection(@"Data Source = kvincentzen.com.mysql\kvincentzen_com;UID=kvincentzen_com;PASSWORD=R6ZzF2BduHv3Mf8o2JzbkzsB;");
-            SqlCommand cmd;
+            var connection = new MySqlConnection("server=kvincentzen.com.mysql.service.one.com; port=3306; database=kvincentzen_com; uid=kvincentzen_com; PASSWORD=R6ZzF2BduHv3Mf8o2JzbkzsB; ");
+            MySqlCommand cmd;
             connection.Open();
 
             try
@@ -37,53 +36,53 @@ namespace DateApp
                 }
             }
         }
-        static void showData()
-        {
-            SqlConnection connection = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = 'C:\Users\ar\documents\visual studio 2015\Projects\DataBaseEksempel\DataBaseEksempel\Database1.mdf'; Integrated Security = True");
+        //static void showData()
+        //{
+        //    SqlConnection connection = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = 'C:\Users\ar\documents\visual studio 2015\Projects\DataBaseEksempel\DataBaseEksempel\Database1.mdf'; Integrated Security = True");
 
-            {
-                connection.Open();
-                using (SqlCommand command = new SqlCommand("SELECT * FROM Employees1", connection))
-                {
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            for (int i = 0; i < reader.FieldCount; i++)
-                            {
-                                Console.WriteLine(reader.GetValue(i));
-                            }
-                            Console.WriteLine();
-                        }
-                    }
-                }
-            }
-        }
-        static void deleteEmployee(int i)
-        {
-            var connection = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = 'C:\Users\ar\documents\visual studio 2015\Projects\DataBaseEksempel\DataBaseEksempel\Database1.mdf'; Integrated Security = True");
-            SqlCommand cmd;
-            connection.Close();
-            cmd = new SqlCommand("DELETE FROM Employees1 WHere id=@i", connection);
+        //    {
+        //        connection.Open();
+        //        using (SqlCommand command = new SqlCommand("SELECT * FROM Employees1", connection))
+        //        {
+        //            using (SqlDataReader reader = command.ExecuteReader())
+        //            {
+        //                while (reader.Read())
+        //                {
+        //                    for (int i = 0; i < reader.FieldCount; i++)
+        //                    {
+        //                        Console.WriteLine(reader.GetValue(i));
+        //                    }
+        //                    Console.WriteLine();
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
+        //static void deleteEmployee(int i)
+        //{
+        //    var connection = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = 'C:\Users\ar\documents\visual studio 2015\Projects\DataBaseEksempel\DataBaseEksempel\Database1.mdf'; Integrated Security = True");
+        //    SqlCommand cmd;
+        //    connection.Close();
+        //    cmd = new SqlCommand("DELETE FROM Employees1 WHere id=@i", connection);
 
-            cmd.Parameters.Add("@i", System.Data.SqlDbType.Int);
-            cmd.Parameters["@i"].Value = i;
-            connection.Open();
+        //    cmd.Parameters.Add("@i", System.Data.SqlDbType.Int);
+        //    cmd.Parameters["@i"].Value = i;
+        //    connection.Open();
 
-            int slettet = cmd.ExecuteNonQuery();
-            if (slettet > 0)
-            {
-                Console.WriteLine("Slettet - TRYK enter.");
-                Console.ReadKey();
-            }
-            else
-            {
-                Console.WriteLine("Ikke fundet - TRYK enter");
-                Console.ReadKey();
-            }
+        //    int slettet = cmd.ExecuteNonQuery();
+        //    if (slettet > 0)
+        //    {
+        //        Console.WriteLine("Slettet - TRYK enter.");
+        //        Console.ReadKey();
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Ikke fundet - TRYK enter");
+        //        Console.ReadKey();
+        //    }
 
-            connection.Close();
-        }
+        //    connection.Close();
+        //}
 
 
     }
